5bfa66e1dcd778dde3a09356d36c639cbb52c133
samukma <nguyensam576@gmail.com>
2021-11-28 20:55:42 +0700

    07

---
+++ 07.py
@@ -0,0 +1,10 @@
+#tra ve y nghia cua cac ham trong py va cac ham tu tao trong py
+
+print(abs.__doc__)
+print(pow.__doc__)
+
+def square(x):
+    "Tra ve binh phuong cua x"
+    return x ** 2
+
+print(square.__doc__)
